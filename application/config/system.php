6c4b68c9724f2bf0a36dde110a79f87ccd2ce355
hanmey <hmy2009@126.com>
2016-01-29 17:29:41 +0800

    菜单

---
+++ application/config/system.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @autor: hanmy
+ * @date: 2016/1/29
+ */
+$config['project_name'] = "农民工管理系统";
+$config['project_logo']  =  array('admin'=>'/public/images/logo.png');
+$config['admin_menu'] = array(
+
+    'service_manage' => array('title' => '服务管理', 'icon' => '/public/images/14309583461214_06.png'),
+    'finance_manage' => array('title' => '财务管理', 'icon' => '/public/images/1430958346122214_06.png'),
+    'housekeeping_manage' => array('title' => '家政服务', 'icon' => '/public/images/14309583461214_08.png'),
+    'resource_manage' => array('title' => '资源管理', 'icon' => '/public/images/14309583461214_18.png'),
+    'system_manage' => array('title' => '系统管理', 'icon' => '/public/images/14309583461214_10.png'));
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -2,46 +2,29 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>物业管理页面</title>
+    <title><?php echo $this->config->item('project_name'); ?></title>
     <link rel="stylesheet" href="/public/css/style.css">
 
     <script type="text/javascript" src="/public/js/jquery.min.js"></script>
 
     <script type="text/javascript" src="/public/js/date.js"></script>
-    <script>
-        $(document).ready(function(){
-            $("#one1,#one2,#one3,#one4,#one5").click(function(){
-                $("#con_one_6").css("display","none")
-            })
-            $("#one0").click(function(){
-                $("#con_one_6").css("display","block")
-            })
-        })
-    </script>
 </head>
+
 <body>
-<div class="top"> <img class="logo" src="images/logo.png">
+
+<div class="top"> <img class="logo" src="<?php $logo = $this->config->item('project_logo'); echo $logo['admin']?>">
     <div class="tab1" id="tab1">
         <div class="menu">
+
             <ul class="nav">
-                <li id="one0" onclick="setTab('one',6)"><a href="#"><img src="images/1430958346122214_06.png">
+                <li id="one0" onclick="setTab('one',6)"><a href="#"><img src="/public/images/1430958346122214_06.png">
                         <p>回到首页</p>
                     </a></li>
-                <li  id="one1" onclick="setTab('one',1)"><a href="#"><img src="images/14309583461214_06.png">
-                        <p>服务管理</p>
+                <?php $list =  $this->config->item('admin_menu'); foreach($list as $key=>$value):?>
+                <li><a href="#"><img src="<?php echo $value['icon'];?>">
+                        <p><?php echo $value['title']; ?></p>
                     </a></li>
-                <li id="one2" onclick="setTab('one',2)"><a href="#"><img src="images/14309583461214_08.png">
-                        <p>财务管理</p>
-                    </a></li>
-                <li id="one3" onclick="setTab('one',3)"><a href="#"><img src="images/14309583461214_16.png">
-                        <p>家政服务</p>
-                    </a></li>
-                <li id="one4" onclick="setTab('one',4)"><a href="#"><img src="images/14309583461214_18.png">
-                        <p>资源管理</p>
-                    </a></li>
-                <li id="one5" onclick="setTab('one',5)"><a href="#"><img src="images/14309583461214_10.png">
-                        <p>系统管理</p>
-                    </a></li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </div>
